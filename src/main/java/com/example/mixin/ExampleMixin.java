6ebefd6a29d79c5fe3d3cdd5c796da018771561a
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-09 18:37:20 +0400

    Create ExampleMixin.java

---
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -0,0 +1,14 @@
+package com.example.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class ExampleMixin {
+	@Inject(method = "updateWindowTitle", at = @At("HEAD"))
+	private void updateWindowTitle(final CallbackInfo ci) {
+	}
+}
