a9fdbd2a2535a15b5ddd595a9bea803a0a22e2fd
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-09 18:36:38 +0400

    Create ExampleMod.java

---
+++ src/main/java/com/example/ExampleMod.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExampleMod implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("example-mod");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
